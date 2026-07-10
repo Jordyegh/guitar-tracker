d709ad4ab34883f5932c2bd9771a64067b19077d
jordyhertogs <jordyegh@gmail.com>
2026-07-10 03:29:16 +0200

    Set up initial Java backend

---
+++ backend/src/test/java/com/jordy/guitartracker/GuitartrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jordy.guitartracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuitartrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
